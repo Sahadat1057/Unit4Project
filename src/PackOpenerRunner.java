876836675f664f4204dadeb9776c4efd24ce81cb
BT_4N2_26 <sifata3@nycstudents.net>
2022-11-09 12:30:27 -0500

    Trying to see how lists work

---
+++ src/PackOpenerRunner.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PackOpenerRunner {
+    public static void main(String[] args) {
+        List myList = new ArrayList();
+        myList.ArrayList("Mike", "John", "Lesly");
+
+        String[] myStrings = new String[] {"Robert Lewandowski, 91, ST","Elem2","Elem3","Elem4","Elem5"};
+        List mylist = Arrays.asList(myStrings );
+
+
+    }
+
+}
