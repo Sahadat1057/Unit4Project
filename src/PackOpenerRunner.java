84fb40f44205300058db0eaa0057b218a407c3ce
BT_4N2_26 <sifata3@nycstudents.net>
2022-11-10 12:01:35 -0500

    Figured that I can use multiple array lists, going be useful when creating different packs

--- src/PackOpenerRunner.java
+++ src/PackOpenerRunner.java
@@ -2,16 +2,24 @@ import java.util.ArrayList;
 public class PackOpenerRunner {
     public static void main(String[] args) {
 
+        String[] allCards = new String[]{"91 Gold, Robert Lewandowski, FC Barcelona, ST", "92 Gold, Kylian Mbappe, Paris Saint Germain, ST", "85 Gold, Rakitic, Sevilla FC, CM"};
+        String[] silverCards = new String[]{"76 Silver, Dan Burn, CB", "79 Silver, Ronald Araujo, CB"};
 
-        String[] myStrings = new String[]{"91 Gold, Robert Lewandowski, Position: ST", "92 Gold, Kylian Mbappe, Position: ST", "85 Gold, Rakitic, Position: CM"};
+        ArrayList<String> allplayerCards = new ArrayList<String>();
+        for(int i = 0; i < allCards.length; i++ ) {
+            allplayerCards.add(allCards[i]);
+        }
 
-        ArrayList<String> playerCards = new ArrayList<String>();
-        for(int i = 0; i < myStrings.length; i++ ) {
-            playerCards.add(myStrings[i]);
+        ArrayList<String> allsilverCards = new ArrayList<String>();
+        for(int i = 0; i < silverCards.length; i++ ) {
+            allsilverCards.add(silverCards[i]);
         }
 
         int randomPicker = (int)(Math.random()*3);
-        System.out.println("Random card picked is: " + playerCards.get(randomPicker));
+        System.out.println("Random Card Picked is: " + allplayerCards.get(randomPicker));
+
+        int silverRandomPicker = (int) (Math.random()*2);
+        System.out.println("Random Card Picked is: " + allsilverCards.get(silverRandomPicker));
     }
 
 }
