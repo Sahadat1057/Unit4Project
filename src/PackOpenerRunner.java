bf48241d743695e43243f309ad47d19527b95584
BT_4N2_26 <sifata3@nycstudents.net>
2022-11-10 11:45:12 -0500

    Created basic array list pulling strings from list, created a basic randomCard picker

--- src/PackOpenerRunner.java
+++ src/PackOpenerRunner.java
@@ -1,13 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
 public class PackOpenerRunner {
     public static void main(String[] args) {
-        List myList = new ArrayList();
-        myList.ArrayList("Mike", "John", "Lesly");
-
-        String[] myStrings = new String[] {"Robert Lewandowski, 91, ST","Elem2","Elem3","Elem4","Elem5"};
-        List mylist = Arrays.asList(myStrings );
 
 
+        String[] myStrings = new String[]{"91 Gold, Robert Lewandowski, Position: ST", "92 Gold, Kylian Mbappe, Position: ST", "85 Gold, Rakitic, Position: CM"};
+
+        ArrayList<String> playerCards = new ArrayList<String>();
+        for(int i = 0; i < myStrings.length; i++ ) {
+            playerCards.add(myStrings[i]);
+        }
+
+        int randomPicker = (int)(Math.random()*3);
+        System.out.println("Random card picked is: " + playerCards.get(randomPicker));
     }
 
 }
